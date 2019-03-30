93d3d38be9ae258854687fac98354f2ec88f3167
darlan <josedarlan961@gmail>
2019-03-30 20:20:37 -0300

    Arquivos para desenvolvimeno web

---
+++ Bootstrap/arquivos iniciais/unidade09/_js/script.js
@@ -0,0 +1,9 @@
+
+
+$(function(){
+  $('.carousel').carousel({
+      interval:3000, pause:"null"
+   });  
+});
+/* pause : " null " quando passar o mouse por cima as imagens não parão de rola */ 
+/* interval é o intervalo que o carousel irá passar as imagens */ 
---
+++ Bootstrap/arquivos iniciais/unidaderevisao10/_js/script.js
@@ -0,0 +1,13 @@
+ 
+
+    $(function(){
+
+           $(".carousel").carousel({
+              interval:2000,
+              pause:"null"
+           });
+         
+    });
+    
+  /* pause : " null " quando passar o mouse por cima as imagens não parão de rola */ 
+/* interval é o intervalo que o carousel irá passar as imagens */ 
